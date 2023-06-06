fd07d6deb966e4efacf57ddf9c9b728ce7dc98f2
Senj0ugahara <kamizu00@mail.ru>
2023-06-06 15:27:35 +0300

    final

--- scripts/main.js
+++ scripts/main.js
@@ -1,81 +1,12 @@
-// function validPhone () {
-//   let reg = /^[\d\+7][\d\(\)\ -]{16,16}\d$/;
-//   let phone = document.querySelector('.input[name="phone"]');
-//   let valid = reg.test(phone.value);
-
-//   if (valid) {
-//     phone.classList.remove('inCorrect');
-//     phone.classList.remove('correct');
-//     phone.classList.add('correct');
-//   } else if (phone.value.length < 18) {
-//     alert('Номер телефона введен неправильно');
-//     phone.classList.remove('correct');
-//     phone.classList.remove('inCorrect');
-//     phone.classList.add('inCorrect');
-//   } else {
-//     alert('Номер телефона введен неправильно');
-//     phone.classList.remove('correct');
-//     phone.classList.remove('inCorrect');
-//     phone.classList.add('inCorrect');
-//     return false;
-//   }
-
-//   return valid;
-// }
-
-// function validName() {
-//   let reg = /[^а-яА-ЯёЁa-zA-Z0-9]+/g;
-//   let name = document.querySelector('.input[name="name"]');
-//   let valid = reg.test(name.value);
-
-//   if (!valid && name.value.length !== 0) {
-//     name.classList.remove('inCorrect');
-//     name.classList.remove('correct');
-//     name.classList.add('correct');
-//   } else {
-//     alert('Введите имя') 
-//     name.classList.remove('correct');
-//     name.classList.remove('inCorrect');
-//     name.classList.add('inCorrect');
-//     return false;
-//   }
-
-//   return valid;
-// }
-
-// function validMessage() {
-//   let reg = /[^а-яА-ЯёЁa-zA-Z0-9]+/g;
-//   let message = document.querySelector('.textarea');
-//   let valid = reg.test(message.value);
-
-//   if (!valid && message.value.length !== 0) {
-//     message.classList.remove('inCorrect');
-//     message.classList.remove('correct');
-//     message.classList.add('correct');
-//   } else {
-//     alert('Сообщение имеет не допустимый формат')
-//     message.classList.remove('correct');
-//     message.classList.remove('inCorrect');
-//     message.classList.add('inCorrect');
-//     return false;
-//   }
-
-//   return valid;
-// }
-
-// function formSubmit(e) {
-//   e.preventDefault();
-//   validPhone();
-//   validName();
-//   validMessage();
-
-//   const phoneNormalize = document.querySelector('.input[name="phone"]').value.replace(/[^0-9+]/g, '');
-// }
-
 const form = document.getElementById('form');
 const formPhone = document.querySelector('.input[name="phone"]');
 const formName = document.querySelector('.input[name="name"]');
 const formMessage = document.querySelector('.textarea');
+const btnOpen = document.getElementById('open');
+
+btnOpen.addEventListener('click', function() {
+  form.classList.toggle('hidden');
+})
 
 function formValidated() {
   let validated = true;
@@ -122,41 +53,27 @@ function formValidated() {
   return validated;
 }
 
-function dataForm() {
+async function dataForm() {
   let formObj = {};
   let formData = new FormData(form);
 
   formData.forEach((value, key) => formObj[key] = value);
-  // formObj.phone.replace(/[^0-9+]/g, '');
-
-  let request = new Request(form.action, {
+  formObj.phone = formObj.phone.replace(/[^0-9+]/g, '');
+  
+  fetch('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
     body: JSON.stringify(formObj),
     headers: {
-      'Content-Type': 'application/json',
+      "Content-type": "application/json; charset=UTF-8"
     }
   })
+  .then(response => response.json())
+  .then(json => {
+    let resultRequest = document.createElement('p');
+    resultRequest.innerHTML = JSON.stringify(json);
+    document.body.append(resultRequest);
+  })
 
-  console.log(request)
-
-  fetch(request).then(
-    function(response) {
-      console.log(response)
-    },
-    function(error) {
-      console.log(error)
-    }
-  );
-
-  console.log('Запрос отправляется')
-
-  // let json = JSON.stringify(formObj);
-
-  // let xhr = new XMLHttpRequest();
-  // xhr.open('POST', 'scripts/formData.json', true)
-  // xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-  // xhr.send(json);
-  // console.log(json)
 }
 
 form.addEventListener('submit', e => {
